d61e97a49f8151735ef7fbdbf72a5c4a5e3b8bb2
Arda Sevinç <ardasevinc.technical@gmail.com>
2024-04-24 21:53:53 +0300

    feat(backend): env typings and schema @fastify/env
    extra: typescript extra stuff required https://github.com/fastify/fastify-env/issues/171

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,17 +1,38 @@
 import Fastify from 'fastify';
-import SimulationRoute from './routes/simulation/index.js';
+import { fastifyEnv, type FastifyEnvOptions } from '@fastify/env';
+import { JSONSchemaType } from 'env-schema';
+import type { Env } from '@/lib/types.js';
+import { readFile } from 'node:fs/promises';
+import SimulationRoute from '@/routes/simulation/index.js';
+
 const fastify = Fastify({
   logger: true,
 });
 
+declare module 'fastify' {
+  interface FastifyInstance {
+    config: Env;
+  }
+}
+
+const envSchema: JSONSchemaType<Env> = JSON.parse(
+  await readFile('./config/env-schema.json', 'utf-8'),
+);
+
+const options = {
+  schema: envSchema,
+  dotenv: true,
+} satisfies FastifyEnvOptions;
+
+await fastify.register(fastifyEnv, options);
 fastify.register(SimulationRoute);
 
-const start = async () => {
+const app = async () => {
   try {
-    await fastify.listen({ host: '0.0.0.0', port: 3006 });
+    await fastify.listen({ host: '0.0.0.0', port: fastify.config.PORT });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
   }
 };
-start();
+app();
