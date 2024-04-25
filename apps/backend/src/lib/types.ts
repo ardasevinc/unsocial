6f7e7192b1e86def164009d61f56988a4f82f013
Arda Sevinç <ardasevinc.technical@gmail.com>
2024-04-26 00:09:17 +0300

    feat(backend): switch to typebox for schema & types
    extra: using TypeBoxTypeProvider for fastify we can define schemas and
    derive typescript types from them easily.

--- apps/backend/src/lib/types.ts
+++ apps/backend/src/lib/types.ts
@@ -1,5 +1,14 @@
-export type Env = {
-  PORT: number;
-  OPENAI_API_KEY: string;
-  ANTHROPIC_API_KEY: string;
-};
+import { Static, Type } from '@sinclair/typebox';
+
+const EnvSchema = Type.Object(
+  {
+    PORT: Type.Number({ default: 3006 }),
+    OPENAI_API_KEY: Type.String(),
+    ANTHROPIC_API_KEY: Type.String(),
+  },
+  { additionalProperties: false },
+);
+
+type Env = Static<typeof EnvSchema>;
+
+export { EnvSchema, Env };
--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,47 +1,33 @@
 import Fastify from 'fastify';
+import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import { fastifyEnv, type FastifyEnvOptions } from '@fastify/env';
 import prismaPlugin from '@/lib/plugins/prisma.js';
-import { JSONSchemaType } from 'env-schema';
-import type { Env } from '@/lib/types.js';
-import { readFile } from 'node:fs/promises';
-import path from 'node:path';
+import { type Env, EnvSchema } from '@/lib/types.js';
 import SimulationRoute from '@/routes/simulation/index.js';
 
 const fastify = Fastify({
   logger: true,
-});
+}).withTypeProvider<TypeBoxTypeProvider>();
 
 declare module 'fastify' {
   interface FastifyInstance {
-    config: Env;
+    env: Env;
   }
 }
 
-const isProduction = process.env.NODE_ENV === 'production';
-const envSchema: JSONSchemaType<Env> = JSON.parse(
-  await readFile(
-    `${path.resolve(
-      process.cwd(),
-      isProduction ? 'dist' : 'src',
-      'config',
-      'env-schema.json',
-    )}`,
-    'utf-8',
-  ),
-);
-
-const options = {
-  schema: envSchema,
+const fastifyEnvOptions = {
+  schema: EnvSchema,
   dotenv: true,
+  confKey: 'env',
 } satisfies FastifyEnvOptions;
 
-await fastify.register(fastifyEnv, options);
+await fastify.register(fastifyEnv, fastifyEnvOptions);
 await fastify.register(prismaPlugin, { config: {} });
 fastify.register(SimulationRoute);
 
 const app = async () => {
   try {
-    await fastify.listen({ host: '0.0.0.0', port: fastify.config.PORT });
+    await fastify.listen({ host: '0.0.0.0', port: fastify.env.PORT });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
