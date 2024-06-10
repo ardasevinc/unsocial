b64eb7758fda40a037c0a24d1dad39e74d024d8a
Arda Sevinç <ardasevinc.technical@gmail.com>
2024-06-10 12:14:52 +0300

    feat(backend): improve server boot & add graceful shutdown plugin

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -3,9 +3,11 @@ import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import { fastifyEnv, type FastifyEnvOptions } from '@fastify/env';
 import prismaPlugin from '@/lib/plugins/prisma.js';
 import fastifySensible from '@fastify/sensible';
+import fastifyGracefulShutdown from 'fastify-graceful-shutdown';
 import { type Env, TEnv } from '@/lib/types.js';
 import SimulationRoute from '@/routes/simulation/index.js';
 
+// TODO: Create a configuration loader function page: 26 in fastify book
 const envToLogger: Record<Env['NODE_ENV'], any> = {
   development: {
     transport: {
@@ -39,18 +41,33 @@ const fastifyEnvOptions = {
   confKey: 'env',
 } satisfies FastifyEnvOptions;
 
-const app = async () => {
+// TODO: Improve server startup and shutdown.
+const bootApp = async () => {
+  /* Recommended booting order:
+   * external plugins
+   * our plugins
+   * decorators
+   * hooks
+   * services - routes etc
+   */
   try {
     await fastify.register(fastifyEnv, fastifyEnvOptions);
     fastify.log.debug(`MODE: ${fastify.env.NODE_ENV}`);
     await fastify.register(prismaPlugin, { config: {} });
-    await fastify.register(fastifySensible);
+    fastify.register(fastifySensible);
+    fastify.register(fastifyGracefulShutdown).after(() => {
+      fastify.gracefulShutdown((signal, next) => {
+        fastify.log.info('Received signal to shutdown: %s', signal);
+        next();
+      });
+    });
     fastify.register(SimulationRoute);
 
+    fastify.log.debug(`PID: ${process.pid}`);
     await fastify.listen({ host: '0.0.0.0', port: fastify.env.PORT });
-  } catch (err) {
-    fastify.log.error(err);
+  } catch (error) {
+    fastify.log.error({ msg: 'Error during server startup', error });
     process.exit(1);
   }
 };
-app();
+bootApp();
