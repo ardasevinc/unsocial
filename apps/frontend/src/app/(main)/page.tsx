cafb44f510d9662e7f799835e02956ef1918d70d
Arda Sevinç <ardasevinc.technical@gmail.com>
2024-06-12 16:35:36 +0300

    feat(frontend): vertical 3 pane layout

--- apps/frontend/src/app/(main)/page.tsx
+++ apps/frontend/src/app/(main)/page.tsx
@@ -1,7 +1,13 @@
+import Feed from '@/components/feed';
+
 export default function Home() {
   return (
-    <main>
-      <div>hello world</div>
-    </main>
+    <>
+      <div className='flex-1'></div>
+      <main className='flex-none w-1/2'>
+        <Feed />
+      </main>
+      <div className='flex-1'></div>
+    </>
   );
 }
