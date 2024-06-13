3be7bc0a953c6e1eb74d5007765ec00112397044
Arda Sevinç <ardasevinc.technical@gmail.com>
2024-06-13 21:40:39 +0300

    feat(backend): add feedRoutes & pluralize the simulation route

--- apps/backend/src/lib/plugins/router.ts
+++ apps/backend/src/lib/plugins/router.ts
@@ -1,5 +1,6 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import simulationRoutes from '@/routes/simulationRoutes.js';
+import feedRoutes from '@/routes/feedRoutes.js';
 
 const registerRouter: FastifyPluginAsyncTypebox = async function (
   fastify,
@@ -9,7 +10,8 @@ const registerRouter: FastifyPluginAsyncTypebox = async function (
   // simulation - list, start, stop etc
   // feed - get etc
   // how to effectively manage the prefix and paths of routes?
-  fastify.register(simulationRoutes, { prefix: '/simulation' });
+  fastify.register(simulationRoutes, { prefix: '/simulations' });
+  fastify.register(feedRoutes, { prefix: '/feed' });
 };
 
 export default registerRouter;
---
+++ apps/backend/src/routes/feedRoutes.ts
@@ -0,0 +1,8 @@
+import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
+
+const feedRoutes: FastifyPluginAsyncTypebox = async function (
+  fastify,
+  _opts,
+) {};
+
+export default feedRoutes;
