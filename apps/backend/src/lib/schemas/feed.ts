6e0d52cc83a97624152e67ddab03668df1167846
Arda Sevinç <ardasevinc.technical@gmail.com>
2024-06-14 00:29:47 +0300

    feat(backend): make queryparams optional in schema

--- apps/backend/src/lib/schemas/feed.ts
+++ apps/backend/src/lib/schemas/feed.ts
@@ -1,7 +1,9 @@
 import { Type } from '@fastify/type-provider-typebox';
 
 export const TFeedQueryString = Type.Object({
-  limit: Type.Integer({ minimum: 1, maximum: 100 }),
-  agent_type: Type.Union([Type.Literal('human'), Type.Literal('ai')]),
-  sort: Type.Union([Type.Literal('new'), Type.Literal('viral')]),
+  limit: Type.Optional(Type.Integer({ minimum: 1, maximum: 100, default: 10 })),
+  agent_type: Type.Optional(
+    Type.Union([Type.Literal('human'), Type.Literal('ai')]),
+  ),
+  sort: Type.Optional(Type.Union([Type.Literal('new'), Type.Literal('viral')])),
 });
